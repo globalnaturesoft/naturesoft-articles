e217ec77e110056d9b0336eeb4d9ddd4b3833afc
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-08-21 19:25:04 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Naturesoft::Articles::Engine.routes.draw do
-  # Frontend
-  get "articles/:category/:id/:title.html" => "articles#detail", as: :detail_articles
-  get ":parent_title/:id/:title.html" => "categories#newest_article", as: :newest_article_categories
-  
   namespace :admin, module: "admin", path: "admin/content" do
     resources :articles do
       collection do
